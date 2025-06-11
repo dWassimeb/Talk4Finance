e974b84c3d0db6674d409de392509d3a623a89a8
Wassime <mohamed-ouassime.el-yamani@openvalue.fr>
2025-06-11 15:20:32 +0200

    adding the base /talk4finance/ to the vit.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/talk4finance/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
